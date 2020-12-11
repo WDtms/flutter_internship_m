d86e5708dbf842aa5123d7605b96ad288e717c7a
WDtms <shepelevaleksey@mail.ru>
2020-12-11 14:37:52 +1000

    Вынес диалог с выбором картинки в отдельный класс

---
+++ lib/presentation/views/current_task_page/add_image_dialog.dart
@@ -0,0 +1,126 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_internship_v2/presentation/pages/flickr_page.dart';
+import 'package:image_picker/image_picker.dart';
+
+class AddImageDialog extends StatelessWidget {
+
+  final Function(String value) addImage;
+  final Map<Color, Color> theme;
+
+  AddImageDialog({this.addImage, this.theme});
+
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: Text('Добавить картинку'),
+      content: SingleChildScrollView(
+        child: ListBody(
+          children: <Widget>[
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: InkWell(
+                onTap: () {
+                  Navigator.pop(context);
+                  Navigator.push(context, MaterialPageRoute(builder: (context1) => FlickrPage(
+                    theme: theme,
+                    addImage: addImage,
+                  )));
+                },
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Expanded(
+                        child: Text(
+                          'Интернет',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff424242),
+                          ),
+                        )
+                    ),
+                    Icon(
+                      Icons.wifi,
+                      size: 28,
+                      color: Color(0xff424242),
+                    )
+                  ],
+                ),
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: InkWell(
+                onTap: () {
+                  _openGallery(context);
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                          'Галерея',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff424242),
+                          ),
+                        )
+                    ),
+                    Icon(
+                      Icons.photo,
+                      size: 25,
+                      color: Color(0xff424242),
+                    ),
+                  ],
+                ),
+              ),
+            ),
+            Divider(),
+            Padding(
+              padding: const EdgeInsets.symmetric(vertical: 4),
+              child: InkWell(
+                onTap: () {
+                  _openCamera(context);
+                },
+                child: Row(
+                  children: [
+                    Expanded(
+                        child: Text(
+                          'Сфотографировать самостоятельно',
+                          style: TextStyle(
+                            fontSize: 18,
+                            color: Color(0xff424242),
+                          ),
+                        )
+                    ),
+                    Icon(
+                        Icons.photo_camera_outlined,
+                        size: 28,
+                        color: Color(0xff424242)
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  _openGallery(BuildContext context) async {
+    Navigator.pop(context);
+    PickedFile picture = await ImagePicker().getImage(source: ImageSource.gallery);
+    if (picture != null) {
+      addImage(picture.path);
+    }
+  }
+
+  _openCamera(BuildContext context) async {
+    Navigator.pop(context);
+    PickedFile picture = await ImagePicker().getImage(source: ImageSource.camera);
+    if (picture != null) {
+      addImage(picture.path);
+    }
+  }
+}
--- lib/presentation/views/current_task_page/flickr_card.dart
+++ lib/presentation/views/current_task_page/flickr_card.dart
@@ -2,9 +2,8 @@ import 'dart:io';
 import 'package:flutter/material.dart';
 import 'package:flutter_internship_v2/data/models/task.dart';
 import 'package:flutter_internship_v2/presentation/bloc/current_task/current_task_cubit.dart';
-import 'package:flutter_internship_v2/presentation/pages/flickr_page.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:image_picker/image_picker.dart';
+import 'package:flutter_internship_v2/presentation/views/current_task_page/add_image_dialog.dart';
 
 class MyFlickrCard extends StatelessWidget {
 
@@ -215,121 +214,13 @@ class MyFlickrCard extends StatelessWidget {
     return showDialog(
       context: context,
       builder: (BuildContext zeroContext){
-        return AlertDialog(
-          title: Text('Добавить картинку'),
-          content: SingleChildScrollView(
-            child: ListBody(
-              children: <Widget>[
-                Divider(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4),
-                  child: InkWell(
-                    onTap: () {
-                      Navigator.pop(context);
-                      Navigator.push(context, MaterialPageRoute(builder: (context1) => FlickrPage(
-                        theme: theme,
-                        addImage: (String value) {
-                          context.bloc<CurrentTaskCubit>().addImage(value);
-                        },
-                      )));
-                    },
-                    child: Row(
-                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                      children: [
-                        Expanded(
-                            child: Text(
-                              'Интернет',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Color(0xff424242),
-                              ),
-                            )
-                        ),
-                        Icon(
-                          Icons.wifi,
-                          size: 28,
-                          color: Color(0xff424242),
-                        )
-                      ],
-                    ),
-                  ),
-                ),
-                Divider(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4),
-                  child: InkWell(
-                    onTap: () {
-                      _openGallery(context);
-                    },
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: Text(
-                              'Галерея',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Color(0xff424242),
-                              ),
-                            )
-                        ),
-                        Icon(
-                            Icons.photo,
-                            size: 25,
-                            color: Color(0xff424242),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-                Divider(),
-                Padding(
-                  padding: const EdgeInsets.symmetric(vertical: 4),
-                  child: InkWell(
-                    onTap: () {
-                      _openCamera(context);
-                    },
-                    child: Row(
-                      children: [
-                        Expanded(
-                            child: Text(
-                              'Сфотографировать самостоятельно',
-                              style: TextStyle(
-                                fontSize: 18,
-                                color: Color(0xff424242),
-                              ),
-                            )
-                        ),
-                        Icon(
-                            Icons.photo_camera_outlined,
-                            size: 28,
-                            color: Color(0xff424242)
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
-            ),
-          ),
+        return AddImageDialog(
+          addImage: (String value) {
+            context.bloc<CurrentTaskCubit>().addImage(value);
+          },
+          theme: theme,
         );
       }
     );
-  }
-
-
-  _openGallery(BuildContext context) async {
-    PickedFile picture = await ImagePicker().getImage(source: ImageSource.gallery);
-    if (picture != null) {
-      context.bloc<CurrentTaskCubit>().addImage(picture.path);
-    }
-    Navigator.pop(context);
-  }
-
-  _openCamera(BuildContext context) async {
-    PickedFile picture = await ImagePicker().getImage(source: ImageSource.camera);
-    if (picture != null) {
-      context.bloc<CurrentTaskCubit>().addImage(picture.path);
-    }
-    Navigator.pop(context);
   }
 }
